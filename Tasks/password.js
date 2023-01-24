a79be5205703f0a5c729180c2c51133c93f64b9a
kovaliovev <kovaliov.evgeniy11@gmail.com>
2023-01-24 19:50:53 +0300

    extract 'random' as a separate function

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,11 +1,13 @@
 'use strict';
 // Generate random password
 
+const getRandomNum = (max) => Math.floor(Math.random() * max);
+
 const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
   let password = '';
   for (let i = 0; i < length; i++) {
-    const index = Math.floor(Math.random() * max);
+    const index = getRandomNum(max);
     password += alphabet[index];
   }
   return password;
